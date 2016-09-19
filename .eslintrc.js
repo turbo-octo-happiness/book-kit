907774dcd57f9dcaa1cc59f4942d34efb10707dd
Robby Kim <robert.st.kim@gmail.com>
2016-09-19 15:39:15 -0400

    added linting file

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "rules": {
+        "arrow-body-style": [ "error", "always" ]
+    }
+};
