91953e1f6d3636df8c984b0672cee0ca9094b76d
Eric Snell <eric_snell21@yahoo.com>
2016-09-20 19:12:31 -0400

    added social login and signup methods

--- public/js/content/login.jsx
+++ public/js/content/login.jsx
@@ -6,6 +6,27 @@ class Login extends React.Component {
     auth: T.instanceOf(AuthService)
   }
 
+  // Social login through github
+  githubLogin() {
+    this.props.auth.login({
+      connection: 'github'
+    }, function(err) {
+      if (err) alert("Something went wrong: " + err.message);
+    });
+  }
+
+  signUp() {
+    // Calls auth0 signup api, sending new account data
+    this.props.auth.signup({
+      connection: 'Username-Password-Authentication',
+      responseType: 'token',
+      email: this.email.value,
+      password: this.password.value
+    }, function(err) {
+      if (err) alert("Something went wrong: " + err.message);
+    });
+  }
+
   handleSubmit(event){
     event.preventDefault()
     // on form submit, sends the credentials to auth0 api
@@ -30,16 +51,25 @@ class Login extends React.Component {
             ref={email => { this.email = email; }}
             placeholder="you@example.com"
           />
+
           <h4>Password</h4>
           <input
             type="text"
             ref={password => { this.password = password; }}
           />
+
           <button
             type="submit"
-            >Sign in
+            >Sign In
           </button>
-
+          <button
+            onClick={this.signup.bind(this)}
+            >Sign Up
+          </button>
+          <button
+            onClick={this.githubLogin.bind(this)}
+            >Login with Github
+          </button>
         </form>
       </div>
     )
