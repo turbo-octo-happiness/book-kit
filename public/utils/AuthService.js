fa7a57402ffffc4bb894a4fd2917a118ff02d5c6
Eric Snell <eric_snell21@yahoo.com>
2016-09-20 23:10:04 -0400

    modified AuthService syntax

--- public/utils/AuthService.js
+++ public/utils/AuthService.js
@@ -1,8 +1,8 @@
-import Auth0 from 'auth0-js'
+import Auth0 from 'auth0-js';
 
 export default class AuthService {
   constructor(clientId, domain) {
-    super()
+    super();
     var options = {
       auth: {
         responseType: 'token',
@@ -10,53 +10,53 @@ export default class AuthService {
           scope: 'openid name email'
         }
       }
-    }
+    };
     // Configure Auth0
-    this.lock = new Auth0Lock(clientId, domain, options)
+    this.lock = new Auth0Lock(clientId, domain, options);
     this.auth0 = new Auth0({
       clientID: clientId,
       domain: domain,
       responseType: 'token'
     });
 
-    this.login = this.login.bind(this)
-    this.signup = this.signup.bind(this)
+    this.login = this.login.bind(this);
+    this.signup = this.signup.bind(this);
   }
 
-  login(params, onError){
+  login(params, onError) {
     // Redirects the call to auth0 instance
-    this.auth0.login(params, onError)
+    this.auth0.login(params, onError);
   }
 
-  signup(params, onError){
+  signup(params, onError) {
     // Redirects the call to auth0 instance
-    this.auth0.signup(params, onError)
+    this.auth0.signup(params, onError);
   }
 
-  parseHash(hash){
+  parseHash(hash) {
     // Uses auth0 parseHash method to extract data from url hash
-    const authResult = this.auth0.parseHash(hash)
+    const authResult = this.auth0.parseHash(hash);
     if (authResult && authResult.idToken) {
-      this.setToken(authResult.idToken)
+      this.setToken(authResult.idToken);
     }
   }
 
-  loggedIn(){
+  loggedIn() {
     // Checks if there is a saved token and it's still valid
-    return !!this.getToken()
+    return !!this.getToken();
   }
 
-  setToken(idToken){
+  setToken(idToken) {
     // Saves user token to localStorage
-    localStorage.setItem('id_token', idToken)
+    localStorage.setItem('id_token', idToken);
   }
 
-  getToken(){
+  getToken() {
     // Retrieves the user token from localStorage
-    return localStorage.getItem('id_token')
+    return localStorage.getItem('id_token');
   }
 
-  logout(){
+  logout() {
     // Clear user token and profile data from localStorage
     localStorage.removeItem('id_token');
   }
