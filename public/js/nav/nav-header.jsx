06161c5d844fd070792ee78f11cc63001a783595
Sierra Gregg <sierragregg@Gmail.com>
2016-09-23 15:44:02 -0500

    Changed https to http

--- public/js/nav/nav-header.jsx
+++ public/js/nav/nav-header.jsx
@@ -15,7 +15,7 @@ const propTypes = {
 function Navbar(props) {
   const lock = new Auth0Lock('6ElpyE9EazmBox2b9PAWytCnFJQTxBCa', 'ericsnell.auth0.com', {
     auth: {
-      redirectUrl: 'https://book-kit.herokuapp.com/#/main',
+      redirectUrl: 'http://book-kit.herokuapp.com/#/main',
       responseType: 'token',
       params: {
         scope: 'openid name identities picture',
