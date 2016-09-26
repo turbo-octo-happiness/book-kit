20d8e9d129be683553ac0c26eefeed6abad1077d
Eric Snell <eric_snell21@yahoo.com>
2016-09-26 15:07:51 -0400

    stupid commit

--- public/js/account/account-display-container.jsx
+++ public/js/account/account-display-container.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import { connect } from 'react-redux';
 import actions from '../redux/actions';
 import Profile from './profile';
+import FolderManagement from './folder-management';
 
 class AccountDisplayContainer extends React.Component {
 
   render() {
+
+
+
     return (
       <div>
         <h1>Account Display</h1>
 
         <Profile profile={this.props.profile} />
 
+        <FolderManagement />
 
       </div>
     );
