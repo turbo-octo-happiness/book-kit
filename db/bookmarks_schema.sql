003bdc513e6b88c4b2579bf83fe6fe0437ad8445
Sierra Gregg <sierragregg@Gmail.com>
2016-09-26 13:57:37 -0500

    Updated schema file

--- db/bookmarks_schema.sql
+++ db/bookmarks_schema.sql
@@ -1,32 +1,39 @@
 DROP TABLE IF EXISTS "bookmark_tags" CASCADE;
 DROP TABLE IF EXISTS "tag" CASCADE;
+DROP TABLE IF EXISTS "user_folder" CASCADE;
 DROP TABLE IF EXISTS "bookmark" CASCADE;
 DROP TABLE IF EXISTS "folder" CASCADE;
 DROP TABLE IF EXISTS "customer" CASCADE;
 
 CREATE TABLE "customer"(
-  customerid INTEGER PRIMARY KEY
+  customerid INTEGER PRIMARY KEY,
+  email VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE "folder"(
   folderid SERIAL PRIMARY KEY,
-  foldername VARCHAR(20) NOT NULL,
-  customerid integer REFERENCES "customer" (customerid)
+  foldername VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE "user_folder"(
+  customerid INTEGER REFERENCES "customer" (customerid),
+  folderid INTEGER REFERENCES "folder" (folderid),
+  PRIMARY KEY (customerid, folderid)
 );
 
 CREATE TABLE "bookmark"(
-  url VARCHAR(250) NOT NULL,
-  title VARCHAR(250) NOT NULL,
+  url VARCHAR(500) NOT NULL,
+  title VARCHAR(500) NOT NULL,
   description TEXT DEFAULT '',
   folderid INTEGER REFERENCES "folder" (folderid),
-  screenshot VARCHAR(100) DEFAULT 'http://placekitten.com/200/300',
+  screenshot VARCHAR(500) DEFAULT 'http://placekitten.com/200/300',
   bookmarkid SERIAL PRIMARY KEY,
   customerid INTEGER REFERENCES "customer" (customerid)
 );
 
 CREATE TABLE "tag"(
   tagid SERIAL PRIMARY KEY,
-  tag VARCHAR(50) NOT NULL
+  tagname VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE "bookmark_tags"(
