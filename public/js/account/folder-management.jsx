8ccd42dddc22ace37ed7c0ff410b9776e388e563
Eric Snell <eric_snell21@yahoo.com>
2016-09-27 11:15:19 -0400

    added TODO

--- public/js/account/folder-management.jsx
+++ public/js/account/folder-management.jsx
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import actions from '../redux/actions';
 import Folder from './folder';
 
+// TODO: Move editing methods to folder component
+
 class FolderManagement extends React.Component {
   constructor() {
     super();
