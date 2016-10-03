78a0f5b41be3fc1a84d6879e90f21aa6328b7519
Eric Snell <eric_snell21@yahoo.com>
2016-10-02 22:36:04 -0400

    page redirects to /main when returning to app if user was authenticated

--- public/js/app-container.jsx
+++ public/js/app-container.jsx
@@ -1,5 +1,7 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { hashHistory } from 'react-router';
+
 import NavbarContainer from './nav/nav-header-container';
 
 // Renders a splash page and the NavbarContainer component if not logged in,
@@ -12,21 +14,30 @@ const propTypes = {
   children: PropTypes.object,
 };
 
-function AppContainer(props) {
-  const display = !props.isAuthenticated ? (
-    <div className="splash-page">
-      <h1>Splash Page Stuff</h1>
-    </div>
-  ) : (
-    props.children
-  );
+class AppContainer extends React.Component {
+  componentWillMount() {
+    console.log('check');
+    if (this.props.isAuthenticated) {
+      hashHistory.push('/main');
+    }
+  }
 
-  return (
-    <div>
-      <NavbarContainer />
-      {display}
-    </div>
-  );
+  render() {
+    const display = !this.props.isAuthenticated ? (
+      <div className="splash-page">
+        <h1>Splash Page Stuff</h1>
+      </div>
+    ) : (
+      this.props.children
+    );
+
+    return (
+      <div>
+        <NavbarContainer />
+        {display}
+      </div>
+    );
+  }
 }
 
 AppContainer.propTypes = propTypes;
