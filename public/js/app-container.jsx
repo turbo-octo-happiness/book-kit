f51da3d74c143530629f3fa0604c2697d0896d44
Eric Snell <eric_snell21@yahoo.com>
2016-10-02 22:48:29 -0400

    moved redirect to navcontainer component

--- public/js/app-container.jsx
+++ public/js/app-container.jsx
@@ -1,43 +1,30 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { hashHistory } from 'react-router';
-
 import NavbarContainer from './nav/nav-header-container';
 
 // Renders a splash page and the NavbarContainer component if not logged in,
 // otherwise renders children (PageContainer component)
-
-// TODO: If isAuthenticated is true, route to /main (hashHistory.push(main))
 
 const propTypes = {
   isAuthenticated: PropTypes.bool,
   children: PropTypes.object,
 };
 
-class AppContainer extends React.Component {
-  componentWillMount() {
-    console.log('check');
-    if (this.props.isAuthenticated) {
-      hashHistory.push('/main');
-    }
-  }
+function AppContainer(props) {
+  const display = !props.isAuthenticated ? (
+    <div className="splash-page">
+      <h1>Splash Page Stuff</h1>
+    </div>
+  ) : (
+    props.children
+  );
 
-  render() {
-    const display = !this.props.isAuthenticated ? (
-      <div className="splash-page">
-        <h1>Splash Page Stuff</h1>
-      </div>
-    ) : (
-      this.props.children
-    );
-
-    return (
-      <div>
-        <NavbarContainer />
-        {display}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <NavbarContainer />
+      {display}
+    </div>
+  );
 }
 
 AppContainer.propTypes = propTypes;
