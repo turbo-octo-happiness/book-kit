2a9311b066a7f9c1111dc5d7fdb8618c188a5ab9
Robby Kim <robert.st.kim@gmail.com>
2016-10-05 17:04:25 -0400

    splash component has image and learn more button

---
+++ public/js/content/splash-page.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function SplashPage() {
+  return (
+    <div className="splash-page">
+      <section className="splash-main">
+
+      </section>
+      <a href="#" className="splash-btn">
+        <span className="learn-more">Learn More</span>
+        <span className="splash-scroll-btn">&#x2228;</span>
+      </a>
+      <section className="splash-features">
+
+      </section>
+      <footer className="splash-footer"></footer>
+    </div>
+  );
+}
