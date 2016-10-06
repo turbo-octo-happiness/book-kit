1fc004f985b76e822e2d52a824923d074c4d9ac6
Robby Kim <robert.st.kim@gmail.com>
2016-10-06 16:46:57 -0400

    finished features section

--- public/js/content/splash-page.jsx
+++ public/js/content/splash-page.jsx
@@ -17,7 +17,41 @@ export default function SplashPage() {
         </span>
       </a>
       <section className="splash-features" id="features">
+        <div className="feature-container">
+          <h1>Organize all of your bookmarks</h1>
+          <div className="features">
+            <div className="features-child">
+              <i className="fa fa-folder fa-2x" aria-hidden="true" />
+              <span>Organize your bookmarks into customizable folders and access the easily with folder filtering</span>
+            </div>
 
+            <div className="features-child">
+              <i className="fa fa-tags fa-2x" aria-hidden="true" />
+              <span>Add re-usable tags to your bookmarks to link resources in different folders</span>
+            </div>
+
+            <div className="features-child">
+              <i className="fa fa-search fa-2x" aria-hidden="true" />
+              <span>Sift through your bookmarks to find what you're looking for instantly with real-time searching</span>
+            </div>
+          </div>
+          <div className="features">
+          <div className="features-child">
+            <i className="fa fa-share-alt fa-2x" aria-hidden="true" />
+            <span>Share folders with other users to maintain a collective library of resources for your co-hort</span>
+          </div>
+
+          <div className="features-child">
+            <i className="fa fa-picture-o fa-2x" aria-hidden="true" />
+            <span>Upload screenshots with your bookmarks to visually remember the resource you saved</span>
+          </div>
+
+          <div className="features-child">
+            <i className="fa fa-laptop fa-2x" aria-hidden="true" />
+            <span>Book Kit is open sourced and the source code is public and open for users to contribute to</span>
+          </div>
+          </div>
+        </div>
       </section>
       <footer className="splash-footer"></footer>
     </div>
